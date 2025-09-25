efbe4f5565e178eacc900d7e10c18bfc01b190b4
flowkite-0689 <157046703+flowkite-0689@users.noreply.github.com>
2025-09-25 14:26:56 +0800

    feat: 添加 strncpy 函数实现
    实现 strncpy 函数并修复 strcpy 函数的代码格式问题，同时更新主函数测试用例。

--- day7hk/strcpy.c
+++ day7hk/strcpy.c
@@ -1,27 +1,45 @@
 #include <stdlib.h>
 #include <stdio.h>
-char *strcpy(char* dest,char* arr)
+char *strcpy(char *dest, char *arr)
 {
-  char *dest_p =dest;
-  for (int i = 0; arr[i]!='\0'; i++)
+  char *dest_p = dest;
+  for (int i = 0; arr[i] != '\0'; i++)
   {
 
-    dest[i]=arr[i];
+    dest[i] = arr[i];
     /* code */
   }
   return dest_p;
-  
-  
+}
+char *strncpy(char *dest, const char *src, size_t n)
+{
+  int i = 0;
+  char *dest_p = dest;
+  for (i = 0; i < n && src[i] != '\0'; i++)
+  {
+    dest[i] = src[i];
+  }
+
+  for (i; i < n; i++)
+  {
+    dest[i] = '\0';
+  }
+
+  return dest_p;
 }
 
 int main(int argc, char const *argv[])
 {
-  
-  char  str[]="hello!!";
 
-  char *str1=(char*)malloc(20);
-  strcpy(str1,str);
+  char str[] = "hello!!";
+
+  char *str1 = (char *)malloc(20);
+  strcpy(str1, str);
   puts(str1);
-  
+
+  // strncpy
+  strncpy(str1, str, 2);
+  puts(str1);
+
   return 0;
 }
